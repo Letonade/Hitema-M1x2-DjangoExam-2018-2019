a7609c1c12539b8eb17b5491d5f394d284e09a25
Letonade <cocoscorpio94@hotmail.fr>
2019-06-07 16:43:07 +0200

    5 Models

--- calc/core/models.py
+++ calc/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class equation(models.Model):
+    chiffre1 = models.CharField(max_length=100)
+    operation = models.CharField(max_length=100)
+    chiffre2 = models.CharField(max_length=100)
+    date = models.DateTimeField(default=timezone.now, verbose_name="Date de parution")
+
+    class Meta:
+        ordering = ['date']
+        
+    def __str__(self):
+        return self.chiffre1 + self.operation + self.chiffre2
--- calc/core/views.py
+++ calc/core/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from datetime import datetime
+from core.models import equation
 from .functions import *
 #from .forms import ContactForm
 
@@ -52,15 +53,16 @@ def core(request):
 		#operation
 	if request.POST.get('operate') ==  "equal":
 		i="equal" 
-		Iredirect = numberRedirect(request)
-		if Iredirect == 0:
-			return redirect(add, request.session.get('number1'), request.session.get('number2'))
-		if Iredirect == 1:
-			return redirect(substract, request.session.get('number1'), request.session.get('number2'))
-		if Iredirect == 2:
-			return redirect(multiply, request.session.get('number1'), request.session.get('number2'))
-		if Iredirect == 3:
-			return redirect(divide, request.session.get('number1'), request.session.get('number2'))
+		if point(request) == 3:
+			Iredirect = numberRedirect(request)
+			if Iredirect == 0:
+				return redirect(add, request.session.get('number1'), request.session.get('number2'))
+			if Iredirect == 1:
+				return redirect(substract, request.session.get('number1'), request.session.get('number2'))
+			if Iredirect == 2:
+				return redirect(multiply, request.session.get('number1'), request.session.get('number2'))
+			if Iredirect == 3:
+				return redirect(divide, request.session.get('number1'), request.session.get('number2'))
 		#operation
 	if request.POST.get('operate') ==  "add":
 		i="add" 
@@ -122,6 +124,10 @@ def core(request):
 
 	number1 = request.session.get('number1');
 	operation1 = request.session.get('operation1');
+	if operation1 == "add" : operation1 = "+"
+	if operation1 == "substract" : operation1 = "-"
+	if operation1 == "divide" : operation1 = "/"
+	if operation1 == "multiply" : operation1 = "x"
 	number2 = request.session.get('number2');
 	return render(request, 'core/core.html', locals())
     
@@ -129,11 +135,12 @@ def core(request):
 def add(request, chiffre1 ,chiffre2):
 	
 	total = int(chiffre1) + int(chiffre2)
+	equation(chiffre1 = request.session.get('number1'), operation = request.session.get('operation1'), chiffre2 = request.session.get('number2')).save()
 	request.session['operation1'] = ""
 	if request.session.get('operation2') != "":
 		request.session['operation1'] = request.session.get('operation2') 
 	request.session['operation2'] = ""
-	request.session['number1'] = total
+	request.session['number1'] = str(total)
 	request.session['number2'] = ""	
 	return redirect(core)
     
@@ -145,7 +152,7 @@ def substract(request, chiffre1 ,chiffre2):
 	if request.session.get('operation2') != "":
 		request.session['operation1'] = request.session.get('operation2') 
 	request.session['operation2'] = ""
-	request.session['number1'] = total
+	request.session['number1'] = str(total)
 	request.session['number2'] = ""	
 	return redirect(core)
     
@@ -157,7 +164,7 @@ def multiply(request, chiffre1 ,chiffre2):
 	if request.session.get('operation2') != "":
 		request.session['operation1'] = request.session.get('operation2') 
 	request.session['operation2'] = ""
-	request.session['number1'] = total
+	request.session['number1'] = str(total)
 	request.session['number2'] = ""	
 	return redirect(core)
     
@@ -169,7 +176,7 @@ def divide(request, chiffre1 ,chiffre2):
 	if request.session.get('operation2') != "":
 		request.session['operation1'] = request.session.get('operation2') 
 	request.session['operation2'] = ""
-	request.session['number1'] = total
+	request.session['number1'] = str(total)
 	request.session['number2'] = ""	
 	return redirect(core)
     
